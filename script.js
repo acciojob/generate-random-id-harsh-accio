adbbcbd3ebcd5d4d320e29a8abefe31547b588a4
harsh-accio <rajputharsh9897@gmail.com>
2025-09-18 11:14:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function makeid(l) {
   // write your code here
+	const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+	for(let i=0; i<l; i++){
+		randomIndex=Math.floor(Math.random()*characters.length);
+		result+=characters[randomIndex]
+	}
+	return result;
 }
+
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
